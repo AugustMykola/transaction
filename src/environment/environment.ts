beb1398d9d6f454648b7644ce8d3dc3f729fd066
Mykola Nosachenko <19hagiri96@gmail.com>
2024-12-08 13:17:07 +0200

    environemnt

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  apiKey: "AIzaSyDIZIEYO2zyC6XnwbvEBx3bU5A1WenbdKA",
+  authDomain: "transactions-test-task.firebaseapp.com",
+  projectId: "transactions-test-task",
+  storageBucket: "transactions-test-task.firebasestorage.app",
+  messagingSenderId: "268864368374",
+  appId: "1:268864368374:web:067baca2f8d310ebf046d7",
+  measurementId: "G-JZ3W3Z5RFK"
+};
